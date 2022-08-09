eafe7093e76930bfb20b31dee804addc024dcc09
Hyunjae Kim <peterhyunjae@naver.com>
2022-08-09 14:33:43 +0900

    [feat] : module working checked

--- back/app/main.py
+++ back/app/main.py
@@ -1,13 +1,17 @@
 from typing import Union
-
 from fastapi import FastAPI
+import mymodule
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/test")
+def test(name):
+    a = mymodule.greeting(name)
+    return {'a' : a}
 
 @app.get("/health")
 def health_check():
